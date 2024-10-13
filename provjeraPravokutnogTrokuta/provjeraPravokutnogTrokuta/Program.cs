f261cc786cd4808c7cadb909f19b99c1eb43aba1
LukaTepsic <166146790+LukaTepsic@users.noreply.github.com>
2024-10-13 13:33:11 +0200

    Add files via upload

---
+++ provjeraPravokutnogTrokuta/provjeraPravokutnogTrokuta/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace provjeraPravokutnogTrokuta
+{
+    class Program
+    {
+        static void Main()
+        {
+            int x = 3;
+            int y = 4;
+            int z = 5;
+
+            if (x > 0 && y > 0 && z > 0)
+            {
+                if (x * x + y * y == z * z)
+                {
+                    Console.WriteLine("Vrijednosti predstavljaju stranice pravokutnog trokuta.");
+                }
+                else
+                {
+                    Console.WriteLine("Vrijednosti ne predstavljaju stranice pravokutnog trokuta.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sve stranice moraju biti veće od 0.");
+            }
+        }
+
+        private string GetDebuggerDisplay()
+        {
+            return ToString();
+        }
+    {
+
